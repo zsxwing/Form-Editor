7421ceba286622b3020568505e39448cf7eb2347
zsxwing <zsxwing@gmail.com>
2012-03-03 15:05:40 +0800

    support to save the submit history

---
+++ history.js
@@ -0,0 +1,53 @@
+function loadHistory() {
+	if (localStorage["history"]) {
+		return JSON.parse(localStorage["history"]);
+	}
+	return [];
+}
+
+function setHistoryMaxSize(size) {
+	localStorage["max_size"]=size;
+}
+
+function getHistoryMaxSize() {
+	if(localStorage["max_size"]) {
+		return localStorage["max_size"];
+	}
+	else {
+		return 20;
+	}
+}
+
+function saveHistory(forms) {
+	localStorage["history"] = JSON.stringify(forms);
+}
+
+function removeHistory(index) {
+	var forms=loadHistory();
+	if (index < forms.length) {
+		forms.splice(index, 1);
+		saveHistory(forms);
+	}
+}
+function truncHistory() {
+	var forms=loadHistory();
+	var maxSize=getHistoryMaxSize();
+	while (forms.length > getHistoryMaxSize()) {
+		forms.pop();
+	}
+	saveHistory(forms);
+}
+
+function addHistory(form) {
+	var forms=loadHistory();
+	forms.unshift(form);
+	var maxSize=getHistoryMaxSize();
+	while (forms.length > getHistoryMaxSize()) {
+		forms.pop();
+	}
+	saveHistory(forms);
+}
+
+function cleanHistory() {
+	saveHistory([]);
+}
